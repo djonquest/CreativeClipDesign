714647016256942ebe32a0eb0587d0dc6f1fce1a
Valdinei <djonquest@gmail.com>
2026-04-26 21:05:38 -0300

    fix: add layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "CreativeClip",
+  description: "SaaS para estilistas com IA",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
